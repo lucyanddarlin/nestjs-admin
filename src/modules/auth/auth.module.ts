94f5c448ee22497e00e0b11e694d70d914a5b2ee
lucyanddarlin <lucykfc1@gmail.com>
2024-12-06 15:49:34 +0800

    feat: 调整 eslint 规则

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import { ISecurityConfig, SECURITY_REG_TOKEN } from '@/config/security.config'
 import { isDev } from '@/global/env'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { ISecurityConfig, SECURITY_REG_TOKEN } from '@/config/security.config'
 import { UserModule } from '../user/user.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
